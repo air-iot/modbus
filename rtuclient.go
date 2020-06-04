d9cf2d9ac32eed6bbf2c1943617986d056496361
wubangbang <hdyzwubangbang@163.com>
2020-06-04 20:15:25 +0800

    rtu添加自定义指令应答包长度

--- rtuclient.go
+++ rtuclient.go
@@ -204,6 +204,8 @@ func calculateResponseLength(adu []byte) int {
 		length += 6
 	case FuncCodeReadFIFOQueue:
 		// undetermined
+	case FuncCodeReadOilRecord:
+		length += 33
 	default:
 	}
 	return length
